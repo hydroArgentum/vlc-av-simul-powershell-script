e8b82c05118d5b1fc24a3d62311d3b62ccc2f71b
Daniel Baek <shiningaura@gmail.com>
2022-03-23 14:50:50 -0400

    Hack a Guaranteed Minimized
    Was planning on opening audio VLC as minimized, but VLC does not honor WindowStyle. Utilized qt-start-minimized instead, with an option to turn off the flag by changing $hackFlag = $false.

--- audioVideoSimulPlay.ps1
+++ audioVideoSimulPlay.ps1
@@ -1,5 +1,8 @@
 $vlcExe = "C:\Program Files\VideoLAN\VLC\vlc.exe"
 
+# See below comment.
+$hackFlag = $true
+
 $videoVolume = '0.700000'
 $videoArg1 = '--no-volume-save'
 $videoArg2 = '--mmdevice-volume='+$videoVolume
@@ -8,10 +11,17 @@ $videoArg3 = '-f'
 $audioVolume = '1.000000'
 $audioArg1 = '--no-volume-save'
 $audioArg2 = '--mmdevice-volume='+$audioVolume
+# Bad hack - VLC does not honor WindowStyle. Autoclose to remove large number of instances.
+$audioArg3 = '--qt-start-minimized'
+$audioArg4 = '--play-and-exit'
 
 $videoArg4 = Read-Host -Prompt "Drag and drop the video file to be played: "
 
-$audioArg3 = Read-Host -Prompt "Drag and drop the audio file to be played: "
+$audioArg5 = Read-Host -Prompt "Drag and drop the audio file to be played: "
 
-& $vlcExe $audioArg1 $audioArg2 $audioArg3
-& $vlcExe $videoArg1 $videoArg2 $videoArg3 $videoArg4
+if ($hackFlag) {
+	Start-Process -FilePath $vlcExe -ArgumentList $audioArg1,$audioArg2,$audioArg3,$audioArg4,$audioArg5
+} else {
+	Start-Process -FilePath $vlcExe -ArgumentList $audioArg1,$audioArg2,$audioArg5
+}
+Start-Process -FilePath $vlcExe -ArgumentList $videoArg1,$videoArg2,$videoArg3,$videoArg4
